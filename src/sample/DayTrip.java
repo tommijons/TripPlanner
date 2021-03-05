406f8a88104dabadde4f7a7f20854f786ab04e23
Daníel Hrafn Ólafsson <55950954+hexvessel@users.noreply.github.com>
2021-03-05 14:40:49 +0000

    DayTrip class, constructor

--- src/sample/DayTrip.java
+++ src/sample/DayTrip.java
@@ -1,5 +1,25 @@
 package sample;
 
-public class DayTrip {
+import java.util.Date;
 
+class DayTrip {
+    private String Location;
+    private Date Date;
+    private int Price;
+    private Boolean handicap;
+    private Boolean Physical;
+    private String[] tags;
+    private Boolean transport;
+    private String URL;
+
+    DayTrip(String l, Date d, int p, Boolean h, Boolean ph, String[] t, Boolean tr, String u) {
+        Location = l;
+        Date = d;
+        Price = p;
+        handicap = h;
+        Physical = ph;
+        tags = t;
+        transport = tr;
+        URL = u;
+    }
 }
