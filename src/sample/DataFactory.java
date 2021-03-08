d9771f54bda91604653313fed413d17698f15414
Daníel Hrafn Ólafsson <55950954+hexvessel@users.noreply.github.com>
2021-03-08 15:57:24 +0000

    DataFactory Uppfærður.

--- src/sample/DataFactory.java
+++ src/sample/DataFactory.java
@@ -1,5 +1,23 @@
 package sample;
 
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 public class DataFactory {
-    // TODO
+    public DataFactory(){
+    }
+    public ArrayList<DayTrip> getDayTrips(){
+        ArrayList<DayTrip> dayTrips = new ArrayList<>();
+        return dayTrips;
+    }
+    public ArrayList<Flight> getFlights(){
+        ArrayList<Flight> flights = new ArrayList<>();
+        return flights;
+    }
+    public ArrayList<Hotel> getHotels(){
+        ArrayList<Hotel> hotels = new ArrayList<>();
+        return hotels;
+    }
 }
