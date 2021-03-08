33976ae66ceae25373b451679c22e6491400c358
Daníel Hrafn Ólafsson <55950954+hexvessel@users.noreply.github.com>
2021-03-08 15:25:46 +0000

    DataFactory búinn til. HotelRoomBooking búið til. HotelRoom búið til.

---
+++ src/sample/HotelRoomBooking.java
@@ -0,0 +1,13 @@
+package sample;
+
+import java.util.Date;
+
+public class HotelRoomBooking {
+    private Hotel hotel;
+    private HotelRoom hotelRoom;
+    private int singleBeds;
+    private int doubleBeds;
+    private Date arrivalDate;
+    private Date departureDate;
+    //VANTAR REST
+}
