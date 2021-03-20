668630e5514cfd89857567a0b0f26c52ed0cdf00
hexvessel <dho1@hi.is>
2021-03-20 17:02:34 +0000

    TravelPackageController added | Review+Controller added

--- src/sample/TravelPackageController.java
+++ src/sample/TravelPackageController.java
@@ -1,4 +1,27 @@
 package sample;
 
 public class TravelPackageController {
+    public void changePackage(TravelPackage travelPackage, Flight flight, Hotel hotel, DayTrip daytrip){
+        travelPackage.setFlight(flight);
+        travelPackage.setHotel(hotel);
+        travelPackage.setDaytrip(daytrip);
+    }
+    public void changeFlight(TravelPackage travelPackage, Flight flight){
+        travelPackage.setFlight(flight);
+    }
+    public void changeHotel(TravelPackage travelPackage, Hotel hotel){
+        travelPackage.setHotel(hotel);
+    }
+    public void changeHotelRoom(TravelPackage travelPackage, HotelRoom hotelRoom){
+        //TODO
+    }
+    public void changeDayTrip(TravelPackage travelPackage, DayTrip dayTrip){
+        travelPackage.setDaytrip(dayTrip);
+    }
+    public void createPackages(Flight[] flights, Hotel[] hotels, DayTrip[] dayTrips){
+        //TODO
+    }
+    public void createCheaperPackages(){
+        //TODO
+    }
 }
