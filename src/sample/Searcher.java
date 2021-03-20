5fd93708ed25bccde779256e4df86778be73a1c6
hexvessel <dho1@hi.is>
2021-03-20 18:12:50 +0000

    Searcher tengt við DataFactory

--- src/sample/Searcher.java
+++ src/sample/Searcher.java
@@ -1,25 +1,26 @@
 package sample;
 
+import javax.xml.crypto.Data;
 import java.util.Date;
 
 public class Searcher {
-
+    DataFactory df = new DataFactory();
     public Flight[] searchForFlights(FlightFilter filter){
         //This is a dummy function
         //TODO Implement Real function
-        return new Flight[0];
+        return df.getFlights().toArray(new Flight[0]);
     }
 
     public Hotel[] searchForHotels(HotelFilter filter){
         //This is a dummy function
         //TODO Implement Real function
-        return new Hotel[0];
+        return df.getHotels().toArray(new Hotel[0]);
     }
 
     public DayTrip[] searchForDayTrips(DayTripFilter filter){
         //This is a dummy function
         //TODO Implement Real function
-        return new DayTrip[0];
+        return df.getDayTrips().toArray(new DayTrip[0]);
     }
 
     public SearchResults searchForPackages(){
@@ -28,12 +29,12 @@ public class Searcher {
         HotelFilter hf = new HotelFilter();//TODO Get Information to construct filter
         Hotel[] hotels = searchForHotels(hf);
         DayTripFilter dtf = new DayTripFilter();//TODO Get Information to construct filter
-        DayTrip[] daytrips = searchForDayTrips(dtf);
-        TravelPackageAssembler assembler = new TravelPackageAssembler(flights, hotels, daytrips);
+        DayTrip[] dayTrips = searchForDayTrips(dtf);
+        TravelPackageAssembler assembler = new TravelPackageAssembler(flights, hotels, dayTrips);
         TravelPackage cheap = assembler.getCheapPackage();
         TravelPackage standard = assembler.getStandardPackage();
         TravelPackage luxury = assembler.getLuxuryPackage();
 
-        return new SearchResults(flights, hotels, daytrips, cheap, standard, luxury);
+        return new SearchResults(flights, hotels, dayTrips, cheap, standard, luxury);
     }
 }
